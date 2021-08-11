e7a3a5af3eafd114d1c053be4ae8b7caa88b5eb0
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-08-11 10:31:27 +0700

    big picture and big text

--- app/src/main/java/com/example/notificationturtorial/MainActivity.java
+++ app/src/main/java/com/example/notificationturtorial/MainActivity.java
@@ -20,6 +20,13 @@ import java.util.Date;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    private static final String TITLE_PUSH_NOTIFICATION = "Vì sao các tỉnh chậm tiêm vắc xin?";
+    private static final String CONTENT_PUSH_NOTIFICATION = "Các tỉnh nói do thiếu nhân lực\n" +
+            "\n" +
+            "Ông Nguyễn Thanh Tùng - giám đốc Sở Y tế tỉnh Hậu Giang - giải thích: Do tỉnh thiếu tủ trữ bảo quản nên chậm trễ trong tiêm vắc xin ngừa COVID-19. Vắc xin nhận từ Bộ Y tế phải gửi tại Viện Pasteur TP.HCM và nhận từng đợt, tiêm hết đợt này mới đem về đợt khác. \n" +
+            "\n" +
+            "Ngoài ra, theo ông Tùng, do triển khai tiêm cho doanh nghiệp, phải đưa lực lượng xuống tận nơi nên nhân lực hạn chế.";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,12 +47,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void sendNotification() {
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_foreground);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher);
 
         Notification notification = new NotificationCompat.Builder(this,MyApplication.CHANNEL_ID)
-                .setContentTitle("Notification Turtorial")
-                .setContentText("Push local notification")
-                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setContentTitle(TITLE_PUSH_NOTIFICATION)
+                .setContentText(CONTENT_PUSH_NOTIFICATION)
+                .setSmallIcon(R.drawable.ic_baseline_notifications_24)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(CONTENT_PUSH_NOTIFICATION))
                 .setLargeIcon(bitmap)
                 .setColor(getResources().getColor(R.color.pink))
                 .build();
@@ -60,13 +68,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void sendNotification_2() {
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.ic_launcher_foreground);
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.hoadeptrai);
 
         Notification notification = new NotificationCompat.Builder(this,MyApplication.CHANNEL_ID)
                 .setContentTitle("Notification Turtorial")
                 .setContentText("Push local notification channel 2")
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setLargeIcon(bitmap)
+                .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap).bigLargeIcon(null))
                 .setColor(getResources().getColor(R.color.purple_200))
                 .build();
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
