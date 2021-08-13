f4dfe8599bb98935905c7ab1bacb95dd810a2dd9
xatkumaulank <70027310+xatkumaulank@users.noreply.github.com>
2021-08-13 17:10:16 +0700

    Custom Notification

--- app/src/main/java/com/example/notificationturtorial/MainActivity.java
+++ app/src/main/java/com/example/notificationturtorial/MainActivity.java
@@ -14,9 +14,11 @@ import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RemoteViews;
 
 import com.example.notificationturtorial.databinding.ActivityMainBinding;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
@@ -46,6 +48,46 @@ public class MainActivity extends AppCompatActivity {
                 sendNotification_2();
             }
         });
+        binding.btnCustomNotification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendCustomNotification();
+            }
+        });
+    }
+
+    private void sendCustomNotification() {
+        //collapsed
+        Uri sound = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.custom_notification);
+
+        RemoteViews remoteViews = new RemoteViews(getPackageName(),R.layout.layout_custom_notification);
+        remoteViews.setTextViewText(R.id.tv_title_custom_notification,"Phó bí thư TP HCM: 'Có thể kéo dài giãn cách đến 15/9'");
+        remoteViews.setTextViewText(R.id.tv_message_custom_notification,"Theo ông Phan Văn Mãi, dự kiến hai ngày nữa TP HCM công bố kế hoạch phòng chống Covid trong 30 ngày tới, tinh thần sẽ tiếp tục giãn cách theo Chỉ thị 16.\n" +
+                "\n" +
+                "Thông tin được Phó bí thư thường trực Thành ủy TP HCM Phan Văn Mãi nói tại cuộc họp báo cung cấp thông tin công tác phòng, chống Covid-19 trên địa bàn trưa 13/8. Cuộc họp diễn ra trong bối cảnh thành phố ghi nhận hơn 137.000 ca nhiễm trong đợt dịch thứ tư, bước qua ngày thứ 36 giãn cách xã hội theo Chỉ thị 16 và 16 tăng cường.");
+
+
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        String strDate = simpleDateFormat.format(new Date());
+        remoteViews.setTextViewText(R.id.tv_time_custom_notfication,strDate);
+
+        //expanded
+        RemoteViews remoteViewsExpanded = new RemoteViews(getPackageName(),R.layout.layout_custom_notification_expand);
+        remoteViewsExpanded.setTextViewText(R.id.tv_title_custom_notification_expand,"Phó bí thư TP HCM: 'Có thể kéo dài giãn cách đến 15/9'");
+        remoteViewsExpanded.setTextViewText(R.id.tv_message_custom_notification_expand,"Theo ông Phan Văn Mãi, dự kiến hai ngày nữa TP HCM công bố kế hoạch phòng chống Covid trong 30 ngày tới, tinh thần sẽ tiếp tục giãn cách theo Chỉ thị 16.\n" +
+                "\n" +
+                "Thông tin được Phó bí thư thường trực Thành ủy TP HCM Phan Văn Mãi nói tại cuộc họp báo cung cấp thông tin công tác phòng, chống Covid-19 trên địa bàn trưa 13/8. Cuộc họp diễn ra trong bối cảnh thành phố ghi nhận hơn 137.000 ca nhiễm trong đợt dịch thứ tư, bước qua ngày thứ 36 giãn cách xã hội theo Chỉ thị 16 và 16 tăng cường.");
+        remoteViewsExpanded.setImageViewResource(R.id.img_custom_notification_expand,R.drawable.hoadeptrai);
+
+
+        Notification notification = new NotificationCompat.Builder(this,MyApplication.CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .setSound(sound)
+                .setCustomContentView(remoteViews)
+                .setCustomBigContentView(remoteViewsExpanded)
+                .build();
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
+        managerCompat.notify(getNotificationId(),notification);
     }
 
     private void sendNotification() {
@@ -71,8 +113,11 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void sendNotification_2() {
+
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.hoadeptrai);
         Uri sound = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.custom_notification);
+
+
         Notification notification = new NotificationCompat.Builder(this,MyApplication.CHANNEL_ID)
                 .setContentTitle("Notification Turtorial")
                 .setContentText("Push local notification channel 2")
@@ -82,6 +127,8 @@ public class MainActivity extends AppCompatActivity {
                 .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(bitmap).bigLargeIcon(null))
                 .setColor(getResources().getColor(R.color.purple_200))
                 .build();
+
+
         NotificationManagerCompat managerCompat = NotificationManagerCompat.from(this);
         managerCompat.notify(getNotificationId(),notification);
 
--- app/src/main/java/com/example/notificationturtorial/MyApplication.java
+++ app/src/main/java/com/example/notificationturtorial/MyApplication.java
@@ -32,7 +32,7 @@ public class MyApplication extends Application {
             //config channel 1
             CharSequence name = getString(R.string.channel_name);
             String description = getString(R.string.channel_description);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            int importance = NotificationManager.IMPORTANCE_MIN;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
             channel.setSound(uri,audioAttributes);
@@ -41,7 +41,7 @@ public class MyApplication extends Application {
             // config channel 2
             CharSequence name_2 = getString(R.string.channel_name_2);
             String description_2 = getString(R.string.channel_description_2);
-            int importance_2 = NotificationManager.IMPORTANCE_DEFAULT;
+            int importance_2 = NotificationManager.IMPORTANCE_HIGH;
             NotificationChannel channel_2 = new NotificationChannel(CHANNEL_ID_2, name_2, importance_2);
             channel_2.setDescription(description_2);
             channel_2.setSound(sound,audioAttributes);
